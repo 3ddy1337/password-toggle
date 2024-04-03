42541a5c51b3d6af51cd1435daa90d8a9c4d1033
3ddy1337 <em@2mrk.de>
2024-04-03 19:50:25 +0200

    restructer code

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// Save element from HTML into JS variables
 const input = document.getElementById("input");
-const btn = document.getElementById("btn");
+const btn = document.getElementById("button");
 
-btn.addEventListener("click", function () {
-  if (input.type === "password") {
+// State
+let showPassword = false;
+
+// HTML update
+function render() {
+  if (showPassword) {
     input.setAttribute("type", "text");
     btn.textContent = "Hide Password";
   } else {
     input.setAttribute("type", "password");
     btn.textContent = "Show Password";
   }
+}
+
+// Event Listener
+btn.addEventListener("click", () => {
+  showPassword = !showPassword;
+  render();
 });
